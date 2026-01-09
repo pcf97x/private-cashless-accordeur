d61c5710debad78d5013dd53dc70205802f41119
Mike Veldwachter <contact@privatecashless.com>
2026-01-08 22:46:35 -0400

    fix(dev): cleanup backend structure (remove nested backend)

--- backend/resources/views/admin/checkins/scan.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <div class="p-6">
-        @if($checkin->checked_in_at)
-            <p>Déjà pointé à {{ $checkin->checked_in_at }}</p>
-        @else
-            <p>Pointage validé ✅</p>
-        @endif
-    </div>
-</x-app-layout>
